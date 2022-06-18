59e8095c81b00c9e25fc8fb4ae6ca86e6fe4ddda
O2D-S <107023180+O2D-S@users.noreply.github.com>
2022-06-18 10:08:17 +0900

    Delete script.js

--- src/main/resources/script.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-console.log('佐藤')
-document.getElementById('form').onsubmit = function() {
-    //検索フォームに入力した内容とプルダウンの情報を含んだURLをhtmlのhrefに代入する
-    location.href = document.getElementById('form').select.value;
-};
