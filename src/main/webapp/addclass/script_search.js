1b81c1a137ce8321dd842e1c028bfda28db27b4b
O2D-S <107023180+O2D-S@users.noreply.github.com>
2022-06-19 12:21:10 +0900

    Add files via upload

---
+++ src/main/webapp/addclass/script_search.js
@@ -0,0 +1,8 @@
+'use strict'
+
+console.log('佐藤')
+document.getElementById('form').onsubmit = function() {
+    //検索フォームに入力した内容とプルダウンの情報を含んだURLをhtmlのhrefに代入する
+    location.href = document.getElementById('form').select.value;
+    console.log('クリックされました')
+};
