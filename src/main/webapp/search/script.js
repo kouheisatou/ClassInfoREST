64f22d736dc0b5ccc64bd1aca4a793d4055951ea
O2D-S <107023180+O2D-S@users.noreply.github.com>
2022-06-21 16:35:52 +0900

    Add files via upload

--- src/main/webapp/search/script.js
+++ src/main/webapp/search/script.js
@@ -9,75 +9,81 @@ document.getElementById('form').onsubmit = function() {
 let classname
 let classCode
 */
-let department_field
+let className //nameだとエラー
+let classCode
+let department
 let grade
 let unitDivision
 let semester
 let campas
+let urlDict={}
 let urlList=[]
 
-/*
+
 document.getElementById('name').onchange = function(){
-    classname=document.getElementById('name').value;
-    console.log(classname);
-    urlList.push(classname);
-    console.log(urlList);
+    className=document.getElementById('name').value;
+    console.log(className);
+    urlDict['name']=className;
+    console.log(urlDict);
 }
+
+
 document.getElementById('classCode').onchange = function(){
     classCode=document.getElementById('classCode').value;
     console.log(classCode);
-    urlList.push(classCode);
-    console.log(urlList);
-}*/
-
-document.getElementById('department_field').onchange = function(){
-    department_field=document.getElementById('department_field').value;
-    console.log(department_field);
-    urlList.push("department_field="+department_field);
-    console.log(urlList);
+    urlDict['classCode']=classCode;
+    console.log(urlDict);
 }
 
 document.getElementById('grade').onchange = function(){
     grade=document.getElementById('grade').value;
     console.log(grade);
-    urlList.push("grade="+grade);
-    console.log(urlList);
+    urlDict['grade']=grade;
+    console.log(urlDict);
 }
 
 document.getElementById('unitDivision').onchange = function(){
     unitDivision=document.getElementById('unitDivision').value;
     console.log(unitDivision);
-    urlList.push("uniDivision="+unitDivision);
-    console.log(urlList);
+    urlDict['unitDivision']=unitDivision;
+    console.log(urlDict);
 }
 
 document.getElementById('semester').onchange = function(){
     semester=document.getElementById('semester').value;
     console.log(semester);
-    urlList.push("semester="+semester);
-    console.log(urlList);
+    urlDict['semester']=semester;
+    console.log(urlDict);
 }
 
 document.getElementById('campas').onchange = function(){
     campas=document.getElementById('campas').value;
     console.log(campas);
-    urlList.push("campas="+campas);
-    console.log(urlList);
+    urlDict['campas']=campas;
+    console.log(urlDict);
 }
 
 function search(){
+
+    // urlDict['name'] = document.getElementById('name').value
+    // urlDict['classCode'] = document.getElementById('classCode').value
+
+    
     //検索フォームに入力した内容とプルダウンの情報を含んだurlListをhtmlのhrefに代入する
-    for(let i=0;i<urlList.length;i++){
-        if(urlList[i]==""){
-            urlList.splice(i);
+    for(let key in urlDict){
+        if(urlDict[key]==""){
+            delete urlDict[key];
         }
     }
+
+    let count=0;
     let URL='';
-    let link="https://tonarinosibahaao.iobb.net:8181/ClassInfoREST/class-info/class/get/?";
-    for(let i=0;i<urlList.length;i++){
-        URL+=urlList[i];
-        if(i==urlList.length-1)break;
+    let link="https://tonarinosibahaao.iobb.net:8181/ClassInfoREST/class/get/?";
+    for(let key in urlDict){
+        URL+=(key+"="+urlDict[key]);
+        if(count==Object.keys(urlDict).length-1)break;
         URL+='&';
+        count++;
     }
     link+=URL;
     console.log(URL);
